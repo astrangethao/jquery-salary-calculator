a8d9f16ecbe1768b193438f2d07ac34e374cbaea
Andrew Nguyen <anguyen1320@gmail.com>
2020-03-14 13:11:58 -0500

    test 1

--- Scripts/client.js
+++ Scripts/client.js
@@ -5,6 +5,12 @@ const employeeForm = [];
 function init() {
   console.log("JQ");
   $(".js-add-employee-form").on("submit", submitEmployeeForm);
+  $(".js-table-body").on("click", ".js-btn-delete", deleteEmployee);
+}
+
+function deleteEmployee() {
+  console.log(this);
+  $(".js-table-body-row").remove();
 }
 
 function submitEmployeeForm(event) {
@@ -20,5 +26,38 @@ function submitEmployeeForm(event) {
   };
 
   employeeForm.push(employeeInfo);
+
   console.log(employeeForm);
+
+  $(".js-input-firstName").val("");
+  $(".js-input-lastName").val("");
+  parseFloat($(".js-input-id").val(""));
+  $(".js-input-title").val("");
+  parseFloat($(".js-input-annualSalary").val(""));
+
+  render();
+}
+
+function render() {
+  $(".js-table-body").empty();
+
+  let totalMonthly = 0;
+
+  for (let i = 0; i < employeeForm.length; i++) {
+    let individualEmployee = employeeForm[i];
+
+    totalMonthly += individualEmployee.employeeAnnualSalary / 12;
+
+    $(".js-table-body").append(`
+    <tr>
+    <td>${individualEmployee.employeeFirstName}</td>
+    <td>${individualEmployee.employeeLastName}</td>
+    <td>${individualEmployee.employeeID}</td>
+    <td>${individualEmployee.employeeTitle}</td>
+    <td>${individualEmployee.employeeAnnualSalary}</td>
+    <td><button class="js-btn-delete">Delete</button></td>
+    </tr>
+    `);
+  }
+  $(".js-total-monthly").text(totalMonthly);
 }
