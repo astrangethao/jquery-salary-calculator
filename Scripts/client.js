119c1f34170aa909e07d0334c34190e0e496a819
Andrew Nguyen <anguyen1320@gmail.com>
2020-03-14 11:43:13 -0500

    submit employee function

--- Scripts/client.js
+++ Scripts/client.js
@@ -1,5 +1,24 @@
 $(document).ready(init);
 
+const employeeForm = [];
+
 function init() {
   console.log("JQ");
+  $(".js-add-employee-form").on("submit", submitEmployeeForm);
+}
+
+function submitEmployeeForm(event) {
+  console.log(event);
+  event.preventDefault();
+
+  const employeeInfo = {
+    employeeFirstName: $(".js-input-firstName").val(),
+    employeeLastName: $(".js-input-lastName").val(),
+    employeeID: parseFloat($(".js-input-id").val()),
+    employeeTitle: $(".js-input-title").val(),
+    employeeAnnualSalary: parseFloat($(".js-input-annualSalary").val())
+  };
+
+  employeeForm.push(employeeInfo);
+  console.log(employeeForm);
 }
